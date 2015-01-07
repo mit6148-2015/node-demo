fe1f15fa846d5424d8887405d16f432a8e8dceb8
Charles Liu <cliu2014@mit.edu>
2015-01-06 22:10:04 -0500

    Demo, with separate answers

--- routes/routes.js
+++ routes/routes.js
@@ -12,24 +12,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* a page to view an individual photo */
-/* GET /photos/:id */
-router.get('/photos/:id', function(req, res) {
-  var photoid = req.param('id');
-  res.render('photo', { url: fakedb[photoid]});
-});
-
-/* a page to add a new photo */
-/* GET /photos/new */
-router.get('/upload', function(req, res) {
-  res.render('new-photo', {});
-});
-
-/* POST /photos */
-router.post('/photos', function(req, res) {
-  var photourl = req.body.url;
-  fakedb[counter] = photourl;
-  res.redirect('/photos/' + counter++);
-});
-
 module.exports = router;
