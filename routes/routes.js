cbc42b1b2a2c4e2785662423a53d84ed6cd1d97f
Charles Liu <cliu2014@mit.edu>
2015-01-07 16:24:57 -0500

    Lecture code

--- routes/routes.js
+++ routes/routes.js
@@ -12,4 +12,27 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET /photos/123 */
+router.get('/photos/:id', function(req, res) {
+  var photoId = req.param('id');  // photoId = 500
+  console.log(photoId);
+  // fakedb[photoId] = ? undefined
+  res.render('photo', { url: fakedb[photoId] });
+});
+
+/* a page to add a new photo */
+/* GET /photos/new */
+router.get('/upload', function(req, res) {
+  res.render('new-photo', {});
+});
+
+/* POST /photos */
+router.post('/photos', function(req, res) {
+  // 1. read the submitted url
+  var submittedUrl = req.body['submitted-url'];
+  // 2. store it.
+  fakedb[counter] = submittedUrl;
+  res.redirect('/photos/' + (counter++));
+});
+
 module.exports = router;
